f37f0b2367cc3dbc6ec3aa43c376e26a45bc161d
alissonjosef <alissonjosef@gmail.com>
2020-01-22 17:30:13 -0300

    alisson02

---
+++ web/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//Componete
+//Estado
+//Propiedade
+
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header title="Titulo 1" />
+      <Header title="Titulo 2" />
+      <Header title="Titulo 4" />
+    </>
+  );
+}
+
+export default App;
