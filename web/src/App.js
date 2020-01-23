b0585d1880438c559dd2bb7ec3c989dbe434eeb0
alissonjosef <alissonjosef@gmail.com>
2020-01-23 17:26:43 -0300

    alisson03

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
-//Componete
-//Estado
-//Propiedade
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
-import Header from './Header';
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App() {
+  const  [devs, setDevs] = useState([]);
+
+  
+
+ 
+  
+  useEffect(() =>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data){
+    
+
+    const response = await api.post('/devs', data)
+
+
+
+    setDevs([...devs, response.data]);
+
+  }
+
   return (
-    <>
-      <Header title="Titulo 1" />
-      <Header title="Titulo 2" />
-      <Header title="Titulo 4" />
-    </>
+    <div id='app'>
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev}/>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
 
